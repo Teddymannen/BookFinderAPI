678f9579af4253c86aadc5f561e3cf21290c1427
Teddymannen <teddy_kross@hotmail.com>
2023-04-21 15:40:20 +0200

    Update generateGenres.js

--- mockdata/collections/generateGenres.js
+++ mockdata/collections/generateGenres.js
@@ -1,15 +1,16 @@
 // GENRES
 import { Genres } from '../../routes/genres.js'
-
+import { faker } from '@faker-js/faker'
+import fs from 'fs'
 
 let genres = []
+const allGenres = [
+    "Action and Adventure", "Biography", "Children's", "Comics and Graphic Novels",
+    "Cookbooks", "Crime and Detective", "Drama", "Essay", "Fantasy", "Historical Fiction",
+    "Horror", "Mystery", "Poetry", "Romance", "Satire", "Science Fiction", "Self-help",
+    "Short Stories", "Suspense and Thrillers", "Travel", "True Crime", "Westerns",
+    "Young Adult", "Religion and Spirituality", "Science and Technology"]
 async function generateGenres(clear = true) {
-    const allGenres = [
-        "Action and Adventure", "Biography", "Children's", "Comics and Graphic Novels",
-        "Cookbooks", "Crime and Detective", "Drama", "Essay", "Fantasy", "Historical Fiction",
-        "Horror", "Mystery", "Poetry", "Romance", "Satire", "Science Fiction", "Self-help",
-        "Short Stories", "Suspense and Thrillers", "Travel", "True Crime", "Westerns",
-        "Young Adult", "Religion and Spirituality", "Science and Technology"]
     if (!clear) {
         return genres = await Genres.find()
     }
@@ -32,4 +33,18 @@ async function generateGenres(clear = true) {
     console.log(`Generated ${genres.length} genres`)
 }
 
-export { generateGenres, genres }
+async function generateGenresJson(clear = true) {
+    if (!clear) {
+        genres = JSON.parse(fs.readFileSync('./json-mockdata/genres.json'))
+    }
+    for (let i = 0; i < allGenres.length; i++) {
+        const genre = {
+            genre: allGenres[i]
+        }
+        genres.push(genre)
+    }
+    console.log(`Generated ${genres.length} genres`)
+    fs.writeFileSync('./json-mockdata/genres.json', JSON.stringify(genres, null, 4))
+}
+
+export { generateGenres, genres, generateGenresJson }
