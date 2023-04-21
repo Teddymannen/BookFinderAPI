1671a1ef5bb2c943719647c4bd719d91fc0c0b07
Teddymannen <teddy_kross@hotmail.com>
2023-04-21 15:40:11 +0200

    Update generateBooks.js

--- mockdata/collections/generateBooks.js
+++ mockdata/collections/generateBooks.js
@@ -3,6 +3,7 @@ import { Books } from '../../routes/books.js'
 import { faker } from '@faker-js/faker'
 import { authors } from './generateAuthors.js'
 import { genres } from './generateGenres.js'
+import fs from 'fs'
 
 let books = []
 async function generateBooks(clear = true) {
@@ -34,4 +35,25 @@ async function generateBooks(clear = true) {
     console.log(`Generated ${books.length} books`)
 }
 
-export { generateBooks, books }
+async function generateBooksJson(clear = true) {
+    if (!clear) {
+        books = JSON.parse(fs.readFileSync('./json-mockdata/books.json'))
+    }
+    for (let i = 0; i < 20; i++) {
+        const numOfAuthors = faker.datatype.number({ min: 1, max: 2 })
+        const numOfGenres = faker.datatype.number({ min: 1, max: 3 })
+        const book = {
+            title: faker.lorem.words(faker.datatype.number({ min: 1, max: 3 })),
+            author: faker.helpers.uniqueArray(authors, numOfAuthors),
+            genre: faker.helpers.uniqueArray(genres, numOfGenres),
+            releaseDate: faker.date.past(),
+            rating: faker.datatype.number({ min: 0, max: 5 })
+        }
+        books.push(book)
+
+    }
+    console.log(`Generated ${books.length} books`)
+    fs.writeFileSync('./json-mockdata/books.json', JSON.stringify(books, null, 4))
+}
+
+export { generateBooks, books, generateBooksJson }
