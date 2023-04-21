a8c802e83e40403668d0d821e402b19c0404988e
Teddymannen <teddy_kross@hotmail.com>
2023-04-21 15:40:07 +0200

    Update generateAuthors.js

--- mockdata/collections/generateAuthors.js
+++ mockdata/collections/generateAuthors.js
@@ -1,6 +1,7 @@
 // AUTHORS
 import { Authors } from '../../routes/authors.js'
 import { faker } from '@faker-js/faker'
+import fs from 'fs'
 
 let authors = []
 async function generateAuthors(clear = true) {
@@ -28,4 +29,21 @@ async function generateAuthors(clear = true) {
     console.log(`Generated ${authors.length} authors`)
 }
 
-export { generateAuthors, authors }
+async function generateAuthorsJson(clear = true) {
+    if (!clear) {
+        authors = JSON.parse(fs.readFileSync('./json-mockdata/authors.json'))
+    }
+    for (let i = 0; i < 20; i++) {
+        const author = {
+            name: faker.name.fullName(),
+            age: faker.datatype.number({ min: 15, max: 120 }),
+            alive: faker.datatype.boolean()
+        }
+        authors.push(author)
+
+    }
+    console.log(`Generated ${authors.length} authors`)
+    fs.writeFileSync('./json-mockdata/authors.json', JSON.stringify(authors, null, 4))
+}
+
+export { generateAuthors, authors, generateAuthorsJson }
